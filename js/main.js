22a502f43880c4e875d7762ad931572b9c993b4f
ssanghavi1 <78315418+ssanghavi1@users.noreply.github.com>
2021-06-22 20:56:09 -0400

    Changes to HW 19

---
+++ js/main.js
@@ -0,0 +1,6 @@
+$('.skill').waypoint(function () {
+    $('.progress .progress-bar').each(function () {
+        $(this).css("width", $(this).attr("aria-valuenow") + '%');
+    });
+}, {offset: '80%'});
+
